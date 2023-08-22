697cbad0ce706ce4f4b0f4517deb00e739a8e6ef
N Ravi Kiran <104976094+ravi955033@users.noreply.github.com>
2023-08-23 04:28:10 +0530

    column swap
    In the attached CSV file swap "Data value" column position with "Subject" column position.
    Subject should become third column.
    And "Data value" should become 8th column.
    
    Interesting read on file operations-
    Python code example to read charcter by character and line by line
    lets assume we have a file named - demofile.txt with inner content written below-
    This is first line in demo file.
    This is second line in demo file.
    This is third line in demo file.
    
    code snippets to read characters by character-
    
    f = open("demofile.txt", "r")
    print(f.read())
    Output-
    This is first line in demo file.
    This is second line in demo file.
    This is third line in demo file.
    
    By default the read() method returns the whole text, but you can also specify how many characters you want to return:
    
    f = open("demofile.txt", "r")
    print(f.read(4))
    
    output =
    This
    
    You can return one line by using the readline() method:
    
    Example
    Read one line of the file:
    
    f = open("demofile.txt", "r")
    print(f.readline())
    Output -
    This is first line in demo file.
    
    By calling readline() two times, you can read the two first lines:
    
    Example
    Read two lines of the file:
    
    f = open("demofile.txt", "r")
    print(f.readline())
    print(f.readline())
    output-
    This is first line in demo file.
    This is second line in demo file.
    
    By looping through the lines of the file, you can read the whole file, line by line:
    
    Example
    Loop through the file line by line:
    
    f = open("demofile.txt", "r")
    for x in f:
    print(x)
    output -
    This is first line in demo file.
    This is second line in demo file.
    This is third line in demo file.
    
    Correct me if m wrong, The OUTPUT should be:
    This is the first line in demo file.
    This is the second line in demo file.
    
    right?

---
+++ column_swap.py
@@ -0,0 +1,22 @@
+
+
+def run():
+    frh = open("machine-readable-business-employment-data-mar-2023-quarter.csv", "r")
+    fwh = open("swapedfile.csv", "w")
+    line = ""
+    for x in frh:
+        x = x.split(",")
+        x[2],x[7] = x[7],x[2]
+        fwh.write(",".join(x))
+
+    frh.close()
+    return fwh
+
+
+outputfile = run()
+outputfile.close()
+
+
+
+
+
